326c10d55bf730c6e0f650f877bb921ce15cee9b
walterh <waltermwfs@gmail.com>
2020-04-15 20:26:08 -0400

    Added route for report and result

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,6 +7,17 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+// accept binary array as input to feed into decoding and learning
+#[post("/api/report", data = "<barray>")]
+fn report(barray: String) -> String {
+    format!("Accepted data [{}]", barray)
+}
+
+#[get("/result")]
+fn result() -> &'static str {
+    "Here's the result and breakdown"
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, report, result]).launch();
 }
