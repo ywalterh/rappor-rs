84286c3b11281228596938b58b76290a1650a46d
wolffdy <wolffdy0@gmail.com>
2020-04-18 16:23:29 +0200

    working towards decoding

--- server/src/decode.rs
+++ server/src/decode.rs
@@ -1,13 +1,44 @@
 use ndarray::array;
 use ndarray_glm::{linear::Linear, model::ModelBuilder, standardize::standardize};
 use ndarray_glm::error::RegressionError;
+use bit_vec::BitVec;
+use ndarray::ArrayBase;
+use ndarray::Array2;
+use ndarray::Array1;
+use ndarray::*;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn to_a1(bv : &BitVec) -> Array1<f32> {
+        Array1::from(
+            bv.iter()
+                .map(|bit| if bit { 1. } else { 0. }).collect::<Vec<f32>>()
+        )
+    }
+
     #[test]
     fn test() -> Result<(), RegressionError> {
+        let bv = BitVec::from_bytes(&[0b10100000, 0b00010010]);
+        let k = bv.len(); // size of filter
+        let h = 1; // number of hash functions
+        let f = 0.5; // permanent response randomizer
+        let p = 0.5; // temporary response randomizer
+        let q = 0.5; // temporary response randomizer
+        let m = 1; // number of cohorts (groups of hash functions used by clients)
+
+        let cohorts = vec![vec![bv]]; // for now, just one cohort of only one client
+
+        let init = || Array1::<f32>::zeros(k);
+       
+        let true_counts_by_cohort = cohorts.iter().map(|cohort| {
+            cohort.iter().fold(init(), |acc, curr_bv| {
+                acc + to_a1(curr_bv)
+            })
+        });
+
+
         // define some test data
         let data_y = array![0.3, 1.3, 0.7];
         let data_x = array![[0.1, 0.2], [-0.4, 0.1], [0.2, 0.4]];
