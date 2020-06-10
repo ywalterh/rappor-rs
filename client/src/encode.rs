3a431fd8979c372747758a7b838bf28bbc41a001
walterh <waltermwfs@gmail.com>
2020-06-10 17:58:05 -0400

    Some light refactoring and initial version of matrix creation

--- client/src/encode.rs
+++ client/src/encode.rs
@@ -1,4 +1,4 @@
-use bloom::{ASMS,BloomFilter};
+use bloom::{BloomFilter, ASMS};
 use rand::Rng;
 
 // This contains the client implementation of RAPPOR
@@ -12,29 +12,39 @@ pub struct Factory {
 
 impl Factory {
     pub fn new(rate: f32) -> Self {
-        Factory { k: 32, rate, f: 0.2 , p: 1, q: 0}
+        Factory {
+            k: 32,
+            rate,
+            f: 0.2,
+            p: 1,
+            q: 0,
+        }
     }
 
-    pub fn process(&self, value: String) -> String {
+    pub fn initialize_bloom_to_bitarray(&self, value: String) -> BloomFilter {
         // step1: hash client's value v onto Bloom filter B of size k using h hash function
         // let's say k is 32
         let mut bf = BloomFilter::with_rate(self.rate, self.k);
-        bf.insert(&value); 
+        bf.insert(&value);
+        return bf;
+    }
+
+    pub fn process(&self, value: String) -> String {
         // this is the B[i] set
-        let bi = bf.bits;
-        
+        let bi = self.initialize_bloom_to_bitarray(value).bits;
+
         // permanent randomized response with f, 1/2 f, 1/2f to 0, 1 - f with Bi
         let mut rng = rand::thread_rng();
         let m = 200;
 
         let mut perm_randomized = Vec::<bool>::new();
-        let k =  (m as f32 * 0.5 * self.f) as u8;
-        let l =  (m as f32 * 1.0 * self.f) as u8;
+        let k = (m as f32 * 0.5 * self.f) as u8;
+        let l = (m as f32 * 1.0 * self.f) as u8;
         for b in bi {
             let random_number = rng.gen_range(0, m);
-            if random_number <= k{
+            if random_number <= k {
                 perm_randomized.push(true);
-            } else if k < random_number  && random_number<= l {
+            } else if k < random_number && random_number <= l {
                 perm_randomized.push(false);
             } else {
                 perm_randomized.push(b);
@@ -66,5 +76,4 @@ mod tests {
         let result = f.process("test".into());
         assert_ne!(result, "");
     }
-
 }
--- decoder/src/decode.rs
+++ decoder/src/decode.rs
@@ -1,78 +1,104 @@
 use bit_vec::BitVec;
+use client::encode;
 use ndarray::array;
 use ndarray::Array1;
 use ndarray::Array2;
 use ndarray::ArrayBase;
 use ndarray::*;
 
+fn to_a1(bv: &BitVec) -> Array1<f32> {
+    Array1::from(
+        bv.iter()
+            .map(|bit| if bit { 1. } else { 0. })
+            .collect::<Vec<f32>>(),
+    )
+}
+
+// take Y and X then produce a model that fit for step 3
+// take ndarray
+// producce a fit result Y of X
+// TODO fix to use lasso here, or at least something similar
+// select candidate strings corresponding to non-zero coefficients.
+fn linear_regression(x: Array2<f32>, y: Array1<f32>) -> Result<(), ErrorKind> {
+    Ok(())
+}
+
+fn create_design_matrx() -> Array2<f64> {
+    let candidate_strings = ["a", "b", "c", "d", "e"];
+    // what's 32?
+    let mut design_matrix = Array2::<f64>::zeros((5, 32));
+
+    for i in 0..candidate_strings.len() {
+        let encode_factory = encode::Factory::new(0.01);
+        let bf = encode_factory.initialize_bloom_to_bitarray(candidate_strings[i].into());
+        let mut row = design_matrix.row_mut(i);
+        for j in 0..row.len() {
+            if bf.bits[i] {
+                row[j] = 1.;
+            } else {
+                row[j] = 0.;
+            }
+        }
+    }
+
+    design_matrix
+}
+
+//Estimate the number of times each bit i within cohort
+//j, tij , is truly set in B for each cohort. Given the
+//number of times each bit i in cohort j, cij was set in
+//a set of Nj reports, the estimate is given by
+//Let Y be a vector of tij s, i  [1, k], j  [1, m].
+fn estimate_y(bv: &BitVec) -> Vec<Array1<f32>> {
+    let k = bv.len(); // size of filter
+    let h = 1.; // number of hash functions
+    let f = 0.5; // permanent response randomizer
+    let p = 0.5; // temporary response randomizer
+    let q = 0.5; // temporary response randomizer
+    let m = 1.; // number of cohorts (groups of hash functions used by clients)
+
+    // Cohorts implement different sets of h hash functions for their Bloom filters, thereby
+    // reducing the chance of accidental collisions of two strings
+    // across all of them.
+    // what's the hash function?
+    let cohorts = vec![vec![bv]]; // for now, just one cohort of only one client
+
+    let init = || Array1::<f32>::zeros(k);
+
+    let reported_counts_by_cohort = cohorts
+        .iter()
+        .map(|cohort| {
+            cohort
+                .iter()
+                .fold(init(), |acc, curr_bv| acc + to_a1(curr_bv))
+        })
+        .collect::<Vec<Array1<f32>>>(); // TODO need to capture number of reports per cohort here too
+    let n = 1.; // cheating here hard coding to 1 report per cohort
+
+    // this is y, pass it along
+    let estimated_true_counts_by_cohort = reported_counts_by_cohort
+        .iter()
+        .map(|counts| {
+            counts.map(|count| (count - (p + 0.5 * f * q - 0.5 * f * p) * n) / ((1. - f) * (q - p)))
+        })
+        .collect::<Vec<Array1<f32>>>();
+
+    estimated_true_counts_by_cohort
+}
+
+// Fit a regular least-squares regression using the selected
+// variables to estimate counts, their standard errors and
+// p-values.
+fn least_square_regression() {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    fn to_a1(bv: &BitVec) -> Array1<f32> {
-        Array1::from(
-            bv.iter()
-                .map(|bit| if bit { 1. } else { 0. })
-                .collect::<Vec<f32>>(),
-        )
+
+    #[test]
+    fn test_create_matrix() {
+        create_design_matrx();
     }
-
-    // take Y and X then produce a model that fit for step 3
-    // take ndarray
-    // producce a fit result Y of X
-    // TODO fix to use lasso here, or at least something similar
-    // select candidate strings corresponding to non-zero coefficients.
-    fn linear_regression(x: Array2<f32>, y: Array1<f32>) -> Result<(), ErrorKind> {
-        Ok(())
-    }
-
-    //Estimate the number of times each bit i within cohort
-    //j, tij , is truly set in B for each cohort. Given the
-    //number of times each bit i in cohort j, cij was set in
-    //a set of Nj reports, the estimate is given by
-    //Let Y be a vector of tij s, i  [1, k], j  [1, m].
-    fn estimate_y(bv: &BitVec) -> Vec<Array1<f32>> {
-        let k = bv.len(); // size of filter
-        let h = 1.; // number of hash functions
-        let f = 0.5; // permanent response randomizer
-        let p = 0.5; // temporary response randomizer
-        let q = 0.5; // temporary response randomizer
-        let m = 1.; // number of cohorts (groups of hash functions used by clients)
-
-        // Cohorts implement different sets of h hash functions for their Bloom filters, thereby
-        // reducing the chance of accidental collisions of two strings
-        // across all of them.
-        // what's the hash function?
-        let cohorts = vec![vec![bv]]; // for now, just one cohort of only one client
-
-        let init = || Array1::<f32>::zeros(k);
-
-        let reported_counts_by_cohort = cohorts
-            .iter()
-            .map(|cohort| {
-                cohort
-                    .iter()
-                    .fold(init(), |acc, curr_bv| acc + to_a1(curr_bv))
-            })
-            .collect::<Vec<Array1<f32>>>(); // TODO need to capture number of reports per cohort here too
-        let n = 1.; // cheating here hard coding to 1 report per cohort
-
-        // this is y, pass it along
-        let estimated_true_counts_by_cohort = reported_counts_by_cohort
-            .iter()
-            .map(|counts| {
-                counts.map(|count| {
-                    (count - (p + 0.5 * f * q - 0.5 * f * p) * n) / ((1. - f) * (q - p))
-                })
-            })
-            .collect::<Vec<Array1<f32>>>();
-
-        estimated_true_counts_by_cohort
-    }
-
-    // Fit a regular least-squares regression using the selected
-    // variables to estimate counts, their standard errors and
-    // p-values.
-    fn least_square_regression() {}
 
     /*
     We need to do regress(X, Y).L2_regularization(lambda)
