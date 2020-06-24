58612c93d3932f4a14024245307ab5db8ce3308c
walterh <waltermwfs@gmail.com>
2020-06-24 15:23:38 -0400

    Commit working OLS implementation

--- decoder/src/linear.rs
+++ decoder/src/linear.rs
@@ -3,6 +3,9 @@ extern crate ndarray_linalg;
 
 use ndarray::*;
 use ndarray_linalg::*;
+use ndarray_rand::*;
+use rand::distributions::Uniform;
+use statrs::distribution::{StudentsT, Univariate};
 
 #[derive(Debug, Default)]
 pub struct Factory {
@@ -16,7 +19,7 @@ pub struct Factory {
     sse: f64,
     coef_standard_errors: Array1<f64>,
     coef_t: Array1<f64>,
-    coef_p: f64,
+    coef_p: Array1<f64>,
 
     residuals: Array1<f64>,
 }
@@ -38,19 +41,26 @@ impl Factory {
         self.degrees_of_freedom_regression = self.num_of_coefficient - 1;
         self.residuals = y - &x.dot(&self.coefficients);
         // not sure what sse is, maybe @dylan knows?
-        self.sse = self.residuals.dot(&self.residuals);
+        self.sse = self.residuals.dot(&self.residuals) / self.degrees_of_freedom_error as f64;
         // into_diag only works for 1D array
         self.coef_standard_errors = (self.sse * &inv_xx).into_diag().mapv(f64::sqrt);
+        println!("coef_se is {}", self.coef_standard_errors);
         self.coef_t = &self.coefficients / &self.coef_standard_errors;
-        /* I don't have cdf yet
         self.coef_p =
-            (1. - cdf(
-                self.coef_t.mapv(f64::abs),
-                self.degrees_of_freedom_regression,
+            (1. - self.cdf(
+                &self.coef_t.mapv(f64::abs),
+                self.degrees_of_freedom_error as f64,
             )) * 2.;
-
-            */
         Ok(())
+    }
+
+    // make cdf works on an array so that we can calculate p-value for all the cofficients
+    pub fn cdf(&self, distribution: &Array1<f64>, df: f64) -> Array1<f64> {
+        distribution.mapv(|x| {
+            println!("x is {}, df is {}", x, df);
+            let n = StudentsT::new(x, 1., df).unwrap();
+            n.cdf(1.)
+        })
     }
 }
 
@@ -59,6 +69,32 @@ mod tests {
     use super::*;
     use ndarray::*;
     use ndarray_linalg::*;
+
+    #[test]
+    fn test_cdf() {
+        let real_weights = array![
+            30.068216996847337,
+            -34.433605927665084,
+            -8.344245977622037,
+            0.,
+            2.56660336442419,
+            3.9890280109854217,
+            3.119234805950629,
+            2.0726629033639665,
+            1.187156437842501,
+            0.42083617225270087,
+            0.,
+            0.,
+            0.,
+            0.,
+            -0.7679035712433514,
+            -1.316257337552551
+        ];
+
+        let f = Factory::new();
+        let result = f.cdf(&real_weights.mapv(f64::abs), 2.);
+        println!("{:?}", result);
+    }
 
     #[test]
     fn test_solve_linalg() -> Result<(), error::LinalgError> {
@@ -83,8 +119,8 @@ mod tests {
             }
         }
 
-        // initialize X 2D array with 60 x 16
-        let mut x_matrix = Array2::<f64>::zeros((x.len(), 16));
+        // initialize X 2D array with 60 x 5
+        let mut x_matrix = Array2::<f64>::zeros((x.len(), 5));
         for j in 0..x.len() {
             let mut row = x_matrix.row_mut(j);
             row[0] = x[j];
@@ -94,72 +130,7 @@ mod tests {
         }
 
         // randomize Y
-        // let Y = x.mapv(f64::sin) + Array::random(60, Uniform::new(0., 0.15));
-        //
-        // use fix Y instead, why introduce sporadic in testing
-        let outputs = array![
-            1.06576338,
-            1.00608589,
-            0.69537381,
-            0.94979894,
-            1.06349612,
-            0.87679492,
-            1.03434863,
-            1.01567311,
-            1.00003454,
-            0.96833186,
-            1.04976168,
-            1.15075133,
-            0.80629667,
-            1.08142497,
-            0.93308857,
-            0.93279605,
-            0.65854724,
-            0.80828129,
-            0.96582538,
-            0.53268764,
-            0.34612837,
-            0.32627941,
-            0.56982979,
-            0.82721666,
-            0.57529033,
-            0.59291348,
-            0.29050974,
-            0.41761115,
-            0.0984859,
-            0.1617371,
-            -0.04009758,
-            -0.15215283,
-            -0.11926686,
-            -0.27933299,
-            -0.07936639,
-            -0.31276815,
-            -0.34670514,
-            -0.52011641,
-            -0.34144842,
-            -0.69758068,
-            -0.54375288,
-            -0.74728915,
-            -0.88405983,
-            -0.86141134,
-            -0.94972624,
-            -0.89793005,
-            -0.94966508,
-            -0.88035836,
-            -0.86628362,
-            -0.99240876,
-            -0.98869355,
-            -0.95115776,
-            -1.08037269,
-            -0.89316682,
-            -0.86818818,
-            -0.95427063,
-            -0.61109018,
-            -0.81343768,
-            -0.94402473,
-            -0.95312111
-        ];
-
+        let outputs = x.mapv(f64::sin) + Array::random(60, Uniform::new(0., 0.15));
         let mut f = Factory::new();
         f.ols(&x_matrix, &outputs)?;
         Ok(())
