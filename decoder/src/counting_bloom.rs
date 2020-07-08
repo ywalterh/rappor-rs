83cf9e4ac5e0eab0247f6a29b36ba133224e3a8e
walterh <waltermwfs@gmail.com>
2020-07-08 17:46:50 -0400

    Update csv parsing and sum bits

--- decoder/src/counting_bloom.rs
+++ decoder/src/counting_bloom.rs
@@ -3,14 +3,15 @@ use ndarray::*;
 use std::collections::HashMap;
 use std::error::Error;
 
+// This method is probably not useful for actual production use
 fn sum_bits(
     data_csv: &str,
     num_cohorts: usize,
     num_bloombits: usize,
-) -> Result<Array2<u8>, Box<dyn Error>> {
+) -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path(data_csv)?;
 
-    let mut data_map = HashMap::<usize, Vec<String>>::new();
+    let mut data_map = HashMap::<usize, usize>::new();
 
     // load in memory first and then. it's only 13M data
     // partitioning data here
@@ -23,13 +24,16 @@ fn sum_bits(
         // so in this case is only cohort_id, irr
         let r = result?;
         let cohort_id = r.get(1).unwrap().parse::<usize>().unwrap();
-        let cohort_vec = data_map.entry(cohort_id).or_insert(Vec::new());
+        let irr = r.get(4).unwrap().parse::<String>().unwrap();
 
-        cohort_vec.push(r.get(2).unwrap().into());
+        let mut counts = count_bits(irr);
+        if data_map.contains_key(&cohort_id) {
+            counts = counts + data_map.get(&cohort_id).unwrap();
+        }
+        data_map.insert(cohort_id, counts);
     }
 
-    let array = Array2::zeros((64, 16));
-    Ok(array)
+    Ok(())
 }
 
 // Return number of positive bits in a string of IRR string
@@ -58,7 +62,7 @@ mod tests {
     #[test]
     fn test_sum_bits() -> Result<(), Box<dyn Error>> {
         let now = Instant::now();
-        sum_bits("tests/case_reports.csv", 64, 16)?;
+        sum_bits("tests/case_reports_shrinked.csv", 64, 16)?;
         println!("Took {}ms", now.elapsed().as_millis());
         Ok(())
     }
