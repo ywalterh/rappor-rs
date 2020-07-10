d54ee4685eac42f8b869189978dc88842dfb9b9b
walterh <waltermwfs@gmail.com>
2020-07-10 14:50:28 -0400

    Update feature matrix

--- decoder/src/feature_matrix.rs
+++ decoder/src/feature_matrix.rs
@@ -7,25 +7,36 @@ use ndarray::Array2;
 // should be able to support both easily
 // matrix is actually also a sum of bits instead of what we used to think
 // an array of bits aren't good enough for compare against, very easily we can get weird results
-pub fn create_design_matrix(num_bloombits: usize, candidate_strings: &[&str]) -> Array2<f64> {
+//
+// Come up with candidate strings and see the distribution matches
+pub fn create_design_matrix(
+    num_bloombits: usize,
+    num_cohorts: usize,
+    candidate_strings: &[&str],
+) -> Array2<f64> {
     //  32 is encode_factory.k ?
     let mut design_matrix = Array2::<f64>::zeros((num_bloombits, candidate_strings.len()));
     for i in 0..candidate_strings.len() {
-        let encode_factory = encode::EncoderFactory::new(1);
-        let irr = encode_factory.encode(1, candidate_strings[i].into());
-        let bits = u32_to_bitvec(irr);
-        assert_eq!(bits.len(), 32, "should be a size 32?");
-        let mut col = design_matrix.column_mut(i);
-        for j in 0..col.len() {
-            if bits[j] {
-                col[j] = 1.;
-            } else {
-                col[j] = 0.;
+        let word: String = candidate_strings[i].into();
+        for cohort in 0..num_cohorts {
+            //for each cohort, generate a count
+            let encode_factory = encode::EncoderFactory::new(1);
+            let irr = encode_factory.encode(cohort as u32, word);
+            let bits = u32_to_bitvec(irr);
+            assert_eq!(bits.len(), 32, "should be a size 32?");
+
+            // instead of having a matrix of a this
+            // row.append(cohort * num_bloombits + (bit_to_set + 1)) but why?
+            //
+            let mut col = design_matrix.column_mut(i);
+            for j in 0..col.len() {
+                let bit = 0;
+                if bits[j] {
+                    bit = 1;
+                }
+                col[j] = (cohort * num_bloombits + (bit + 1)) as f64;
             }
         }
-
-        // instead of having a matrix of a this
-        // row.append(cohort * num_bloombits + (bit_to_set + 1))
     }
 
     design_matrix
@@ -50,7 +61,7 @@ mod tests {
     #[test]
     fn test_create_matrix() {
         let candidate_strings = super::test_candidate_strings;
-        let matrix = super::create_design_matrix(16, candidate_strings);
+        let matrix = super::create_design_matrix(16, 32, candidate_strings);
 
         // uncomment if want sanity check on matrix
         // println! {"resuling matrix is {}", matrix};
